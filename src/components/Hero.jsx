ed83733ccda858109ee7b213ebb0ac6feb0e9981
shyamkumar0707 <shyam.kumar.0707@gmail.com>
2023-10-08 18:57:00 +0530

    New AI Project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/shyamkumar0707/AI-PageSummary.git", "_blank")
           }
           className='black_btn'
         >
